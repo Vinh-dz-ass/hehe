6cf846a52a56e090d70379b638a490ac026140f4
Vinh-dz-ass <vinhkye96@gmail.com>
2025-07-14 01:43:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function yes() {
   document.body.style.background = "linear-gradient(to top right, #f8bbd0, #f48fb1)";
 }
 function no() {
-  document.getElementById("response").innerHTML = "Không, em thích anh rất";
+  document.getElementById("response").innerHTML = "Mình cứ thế thôi, hẹ hẹ";
   document.body.style.background = "linear-gradient(to top right, #cfd8dc, #90a4ae)";
 }
