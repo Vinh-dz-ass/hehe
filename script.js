8b6c47b96c5d533446e3bdc6800b7fbdcc765e2a
Vinh-dz-ass <vinhkye96@gmail.com>
2025-07-14 01:32:22 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function yes() {
+  document.getElementById("response").innerHTML = "💕 Anh hạnh phúc lắm. Cảm ơn em nhiều lắm! 💕";
+  document.body.style.background = "linear-gradient(to top right, #f8bbd0, #f48fb1)";
+}
+function no() {
+  document.getElementById("response").innerHTML = "🥲 Không sao đâu... Cảm ơn em vì đã lắng nghe.";
+  document.body.style.background = "linear-gradient(to top right, #cfd8dc, #90a4ae)";
+}
